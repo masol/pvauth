991fbea915c5d64dd0aa8dbdb6090cbca3eebf58
lizhutang <lizhutang@spolo.org>
2022-09-22 02:56:29 +0800

    支持了init及migrate命令

--- index.js
+++ index.js
@@ -10,9 +10,24 @@
 // File: index
 'use strict'
 const fp = require('fastify-plugin')
+const path = require('path')
 
 module.exports = fp(async function (fastify, opts = {}) {
-  const { soa } = fastify
+  const { soa, _, $, log } = fastify
   await soa.get('knex')
+  const ojs = await soa.get('objection')
+  const files = await $.glob(`${path.join(__dirname, 'src', 'models')}/**/*.js`)
+  for (let i = 0; i < files.length; i++) {
+    // console.log(`file ${i} = ${files[i]}`)
+    try {
+      const m = require(files[i])
+      await m.setup(fastify, ojs)
+    } catch (e) {
+      log.error("加载Schema'%s'时发生错误:%s", files[i], e)
+    }
+  }
+  if (_.isObject(fastify.runcmd)) { // runcmd mode.
+    return await require('./src/cmds').run(fastify, opts)
+  }
   fastify.log.debug('auth opts=', opts)
 }, { fastify: '4.x' })
---
+++ src/cmds/migrate/v1.js
@@ -0,0 +1,80 @@
+/// //////////////////////////////////////////////////////////////////////////
+//                                                                          //
+//  本文件是WIDE2.0的组成部分.                                               //
+//                                                                         //
+//  WIDE website: http://www.wware.org/                                    //
+//  WIDE website: http://www.prodvest.com/                                 //
+//  License : WWARE LICENSE(https://www.wware.org/license.html)            //
+/// /////////////////////////////////////////////////////////////////////////
+// Created On : 20 Sep 2022 By 李竺唐 of SanPolo.Co.LTD
+// File: v1
+
+const USER = 'users'
+// const OAUTH = 'oauth'
+// const AUTH = 'auth'
+module.exports = function (fastify, opts) {
+  return {
+    async up (knex) {
+      // await knex.raw('CREATE SCHEMA IF NOT EXISTS ??', AUTH)
+      // return knex.schema.withSchema(AUTH)
+      return knex.schema
+        .createTable(USER, function (table) {
+          table.uuid('id', { primaryKey: true }).defaultTo(knex.raw('gen_random_uuid()'))
+          // 帐号名，可空。
+          table.string('accountName', 128).unique().nullable()
+          // 密码md5。
+          table.string('password', 128).nullable()
+          // 昵称
+          table.string('nickName', 128).nullable()
+          // 显示用真实名
+          table.string('commonName', 32).nullable()
+          // 邮箱地址
+          table.string('email', 128).unique().nullable()
+          // 邮箱地址已验证。
+          table.boolean('emailVerified').defaultTo(false)
+          // 手机号码
+          table.string('mobile', 32).unique().nullable()
+          // 手机号码已验证。
+          table.boolean('mobileVerified').defaultTo(false)
+          // 电话号码
+          table.string('phone', 32).unique().nullable()
+          // 电话号码已验证。
+          table.boolean('phoneVerified').defaultTo(false)
+          // 真实姓名
+          table.string('name', 32).nullable()
+          // 真实姓名已验证(身份证验证)
+          table.boolean('nameVerified').defaultTo(false)
+          // 身份证号
+          table.string('idcard', 64).unique().nullable()
+          // 身份证号已验证(身份证验证)
+          table.boolean('idcardVerified').defaultTo(false)
+          // avatar(URL)
+          table.text('avatar').nullable()
+          // 所属角色,采用数字数组。
+          table.specificType('role', 'integer ARRAY').nullable()
+          // 所属组数组,采用数字数组。
+          table.specificType('group', 'integer ARRAY').nullable()
+        })
+    },
+    async down (knex) {
+      const { log, runcmd, _ } = fastify
+      // const schema = await knex.schema.withSchema(AUTH)
+      // const exist = await schema.hasTable(USER)
+      const exist = await knex.schema.hasTable(USER)
+      if (exist) {
+        if (!runcmd.force) {
+          const countInfo = await knex.table(USER).count()
+          const count = (_.isArray(countInfo) && countInfo.length > 0) ? countInfo[0].count : 0
+          // console.log('count=', count)
+          if (count > 0) {
+            console.log('error!!!')
+            const msg = `${USER}表中有${count}条数据，请添加-- --force参数来强制删除表格。`
+            log.error(msg)
+            throw new fastify.error.ServiceUnavailableError(msg)
+          }
+        }
+        await knex.schema.dropTable(USER)
+      }
+    }
+  }
+}
